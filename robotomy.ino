ed3676f617bb0dee0d868bd0e96e97cd85d6e108
Josh Bertram <jrjbertram@gmail.com>
2014-08-20 21:15:45 -0500

    Moved the serial/ethernet muxing classes off to separate file

--- robotomy.ino
+++ robotomy.ino
@@ -16,6 +16,9 @@
 // Elapsed time measurement
 #include "elapsedMillis.h"
 
+// Helper classes to simplify serial + telnet input/output.
+#include "StreamUtils.h"
+
 
 // Pin mappings - these will eventually be the real map
 
@@ -186,109 +189,6 @@ void setup()
   
 }
 
-
-class ClientStream : public Stream {
-  public:
-  ClientStream( Adafruit_CC3000_ClientRef * client ) { _client = client; haveCachedData = false;}
-  
-  int available() { return _client->available(); }
-  size_t write( uint8_t data) { return _client->write( data ); }
-  int read(  ) { 
-     if( haveCachedData )
-     { 
-        haveCachedData = false;
-        return cachedData;
-     }
-     else
-     {
-       return _client->read( ); 
-     }
-  }
-  
-  int peek( ) {
-    if( haveCachedData )
-    {
-      Serial.print( "peek called when already have cached data.. need an array?" );
-      while( 1 ) ;
-    }
-    else
-    {
-      cachedData = read();
-      haveCachedData = 1;
-    }
-    return cachedData;
-  } 
-  
-  void flush() {}
-
-  
-  private:
-  Adafruit_CC3000_ClientRef * _client;
-  bool haveCachedData;
-  uint8_t cachedData;
-  
-};
-
-// Class that will let me mux output on two streams
-class MuxStream : public Stream {
-  public:
-  MuxStream( Stream * a, Stream * b ) { _a = a; _b = b;}
-  MuxStream( Stream * a  ) { _a = a; _b = NULL;}  
-  MuxStream() { _a = NULL; _b = NULL;}  
-  MuxStream(const MuxStream& copy) { _a = copy._a; _b = copy._b; }
-  void operator=(const MuxStream& copy) { _a = copy._a; _b = copy._b; }
-  
-  int available() { 
-    if( _a && _a->available() )
-    {
-      return 1;
-    }
-    
-    if( _b && _b->available() )
-    {
-      return 1;
-    }    
-  }
-  
-  size_t write( uint8_t data) {
-    if( _a ) { _a->write( data ); }
-    if( _b ) { _b->write( data ); }
-    return 1; 
-  }
-  
-  int read(  ) { 
-     if( _a && _a->available() )
-     { 
-       return _a->read( ); 
-     }
-     
-     if( _b && _b->available() )
-     { 
-       return _b->read( ); 
-     }
-  }
-  
-  int peek( ) {
-
-     if( _a && _a->available() )
-     { 
-       return _a->peek( ); 
-     }
-     
-     if( _b && _b->available() )
-     { 
-       return _b->peek( ); 
-     }
-  } 
-  
-  void flush() {}
-
-  
-  private:
-    Stream * _a;
-    Stream * _b;
-  
-};
 
 elapsedMillis elapsed;
 
